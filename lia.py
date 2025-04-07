88884336b6dacd3cb4ef84396d42f349975cfa7c
Anatolii Radchenko <radchenko.anatoliy@gmail.com>
2025-04-07 19:19:38 +0200

    first ansible playbook tested.

--- lia.py
+++ lia.py
@@ -1,27 +1,64 @@
+import requests
+import sys
+import subprocess
+
 import json
 import yaml
-import subprocess
-import sys
 import os
-import platform
 import time
 
-os_type = platform.system() # Windows Linux
-c = { 
-    "red":'\033[91m',
-    "green":'\033[92m',
-    "grey":'\033[90m'
-}
 
-_lang={}
-cend = '\033[0m'
+for _i in ["ansible","terraform","modules"]: os.makedirs(_i, exist_ok=True)
+sys.path.append("modules")
+def load_module_from_url(url, module_name):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(f"modules/{module_name}.py", "wb") as f:
+            f.write(response.content)
+        sys.path.append(".")
+        imported_module = __import__(module_name)
+        return imported_module
+    except Exception as e:
+        print(f"Errore durante il download/importazione del modulo: {e}")
+        return None
+def prepare(target):
+    match target:
+        case 'ansible':
+            # Crea il file di configurazione di Ansible
+            if not os.path.exists("ansible/ansible.log"):
+                with open("ansible/ansible.log", 'w') as f: f.write("\n")
+            if not os.path.exists("ansible/inventory"):
+                inventory_file = os.path.join("ansible", "inventory")
+                with open(inventory_file, 'w') as f:
+                    f.write("[localhost]\nlocalhost ansible_connection=local\n")
+            if not os.path.exists("ansible/ansible.cfg"):
+                libra.debug('create ansible.cfg')
+                ansible_config = [
+                    "[defaults]",
+                    f"inventory = inventory",
+                    f"log_path = {os.path.join('ansible', 'ansible.log')}",
+                    "host_key_checking = False",
+                    "remote_user = root",
+                    "gather_facts = True",
+                    "fact_caching = jsonfile",
+                    "fact_caching_connection = /tmp/.ansible_fact_cache"
+                ]
+                config_file = os.path.join("ansible", "ansible.cfg")
+                with open(config_file, 'w') as f:
+                    f.write('\n'.join(ansible_config))
 
-def lang(_tag): return _tag if _tag not in _lang else _lang[_tag] 
-def _print( _msg, _color = 'green' ): print( c[ _color ] + _msg + cend )
 
-def clearScreen(): 
-    os.system('cls' if os_type == "Windows" else 'clear')
-    _print(lang('title'))
+url = "https://raw.github.com/AnatoliiRadchenko/pythonLibra/main/Libra.py"
+Libra = load_module_from_url(url, "Libra")
+libra = Libra.Libra(True)
+
+
+def changeLanguage(_arg): libra.changeLanguage(_arg)
+def clearScreen():
+    libra.clearScreen()
+    libra.cprint(libra.lang('title'))
+def _print( _msg, _color = 'green' ): libra.cprint(_msg,_color)
 
 def load_menu_config(config_file):
     if config_file.endswith('.json'):
@@ -33,83 +70,67 @@ def load_menu_config(config_file):
     else:
         raise ValueError("Formato del file non supportato. Usare JSON o YAML.")
 
-def execute_ansible(playbook_path, inventory=None):
-    command = ['ansible-playbook']
-    if inventory:
-        command.extend(['--inventory', inventory])
-    command.append(playbook_path)
-    subprocess.run(command)
-
-def execute_terraform(commands):
-    terraform_cmd = ['terraform'] + commands
-    subprocess.run(terraform_cmd)
-
-def execute_shell_command(command):
-    subprocess.run(command)
-
-
-
 def show_menu(menu_items, current_level=0, indent=""):
     for index,item in enumerate(menu_items):
         _color = 'green' 
         if 'type' not in item and 'items' not in item:
              _color = 'grey'
-        _print(f"{indent} {index+1}. {lang(item if 'text' not in item else item['text'])}", _color)
-        '''if 'items' in item:
-            show_menu(item['items'], current_level + 1, indent + "  ")
-        else:
-            pass'''
-
-def changeLanguage(_arg):
-    global _lang 
-    with open('lang/'+_arg+'.json', 'r') as f:
-        _lang  = json.load(f)
+        _print(f"{indent} {index+1}. {libra.lang(item if 'text' not in item else item['text'])}", _color)
 
 def handle_selection(menu_config, _level, menuPre = None):
     while True:
         clearScreen()
-        _print("\n"+lang("selectOption")+"\n")
+        _print("\n"+libra.lang("selectOption")+"\n")
         show_menu(menu_config["items"],_level)
         
         try:
-            selected = input(c['green']+"\n"+lang('insertNumberOr')+(lang('exit') if _level == 0 else lang('mainMenu'))+"): ")
+            selected = input(libra.colors['green']+"\n"+libra.lang('insertNumberOr')+(libra.lang('exit') if _level == 0 else libra.lang('mainMenu'))+"): ")
             if selected.lower() == '0':
                 if _level==0:
-                    _print(lang('bye'))
+                    _print(libra.lang('bye'))
                     break
                 else:
                     _level=0
                     menu_config=menuMain
             else:
                 if 'type' not in menu_config["items"][int(selected)-1] and 'items' not in menu_config["items"][int(selected)-1]: 
-                    _print(lang("menuIsDisabled"),'red')
+                    _print(libra.lang("menuIsDisabled"),'red')
                     time.sleep(1)
                 else:
                     menu_config=menu_config["items"][int(selected)-1]
-                    if 'type' not in menu_config and 'items'  in menu_config:
+                    if 'type' not in menu_config and 'items' in menu_config:
                          menu_config['type']='submenu'
                     print(menu_config)
                     match menu_config['type']:
+                        case 'ansible':
+                            subprocess.run(["sh", "-c", "sudo -v"], capture_output=True, text=True)
+                            prepare('ansible')
+                            result = subprocess.run(["sh", "-c", "cd ansible && ansible-playbook "+menu_config['path']])
+                            libra.pressEnter()
+                            _level=0
+                            menu_config=menuMain
                         case 'submenu': 
                             _level +=1
                         case 'function':
-                            #print(menu_config)
                             globals()[ menu_config["function"] ](*menu_config["arg"])
                             menu_config=menuMain
         except KeyboardInterrupt:
-            _print("\n"+lang('exitProgram')+'.','red')
+            _print("\n"+libra.lang('exitProgram')+'.','red')
             break
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        _print(lang('usage'), 'red')
-        sys.exit(1)
-    
-    config_file = sys.argv[1]
+        if not os.path.exists("menu.json"): 
+            _print(libra.lang('usage'), 'red')
+            sys.exit(1)
+        else: config_file = "menu.json"
+    else: config_file = sys.argv[1]
     try:
-        changeLanguage('it')
+        libra.changeLanguage('it')
         menu_config = load_menu_config(config_file)
         menuMain=menu_config
         handle_selection(menu_config,0)
     except Exception as e:
-        _print(lang("error")+": {e}", 'red')
+        _print(libra.lang("error")+": ", 'red')
+        print(e)
+
---
+++ modules/Libra.py
@@ -0,0 +1,48 @@
+#
+# author: Anatolii Radchenko
+# description: python library
+# @ Rome 2025
+#
+class Libra:
+  def __init__(self,debug=False):
+    self.isDebug = debug
+    self.colors = { 
+        "red":'\033[91m',
+        "green":'\033[92m',
+        "grey":'\033[90m',
+        "cend": '\033[0m'
+    }
+    self.dbLang={}
+    self.importModule("os")
+    self.importModule("json")
+    self.importModule("platform")
+    self.importModule("subprocess")
+    self.osType = platform.system() # Windows Linux
+
+    
+  def changeLanguage(self,_arg):
+    with open('lang/'+_arg+'.json', 'r') as f:
+        self.dbLang  = json.load(f)
+
+  def cprint( self,_msg, _color = 'green' ): print( self.colors[ _color ] + _msg + self.colors['cend'] )
+  def clearScreen(self): 
+      os.system('cls' if self.osType == "Windows" else 'clear')
+  def lang(self,_tag): return _tag if _tag not in self.dbLang else self.dbLang[_tag] 
+  def pressEnter(self):
+    _input = 1
+    while _input!='': _input=input('press Enter')  
+  def debug(self,msg):
+    if(self.isDebug): print(': '+msg)
+  def su(self):
+    self.debug('su')
+    subprocess.run(["sh", "-c", "sudo -v"])
+  def importModule(self,name):
+    try:
+        globals()[name] = __import__(name)  # Tentativo di importare il modulo
+    except ImportError:
+        self.debug("Il modulo 'requests' non è installato. Installo automaticamente...")
+        #from subprocess import call
+        self.su()
+        result = subprocess.run(["sh", "-c", "sudo dnf install -y pip || sudo apt install pip -y"],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+        result = subprocess.run(["sh", "-c", "pip install "+name],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+        globals()[name] = __import__(name)
